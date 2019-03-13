9589faaa2a99bd63e5023c1290fe94ddc03e0dcc
Jared Barrow <jaredb96@Jareds-MacBook.local>
2019-03-13 17:49:46 -0400

    Added list to linked list method and singly linked node class

--- InterviewSerializer/src/serial/Serializer.java
+++ InterviewSerializer/src/serial/Serializer.java
@@ -1,31 +1,66 @@
 package serial;
 
 public class Serializer {
-	
-	private class ListNode<T> {
-		private T value; 
-		private ListNode next; 
+	/**
+	 * LinkedList class
+	 * @author jaredb96
+	 *
+	 */
+	public static class ListNode{
+		int val;
+		ListNode next;
 		
-		/**
-		 * Basic constructor 
-		 * @param val
-		 */
-		public ListNode(T val){
-			value = val;
+		
+		ListNode(){
+			val = -1;
+			next = null;
+		}
+		ListNode(int x){
+			val = x;
 			next = null;
 		}
 		
-		public ListNode(T val, ListNode n) {
-			value = val;
-			next = n;
-		}
 		
 		
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] test = {1, 2, 3, 4, 5};
+		ListNode testLinked = listToLinkedList(test);
+		ListNode iterator = testLinked;
+		for (int i = 0; i < 5; i++) {
+			if (i == 4) {
+				System.out.print(iterator.val);
+			}
+			else {
+				System.out.print(iterator.val + "->");
+			}
 
+			iterator = iterator.next;
+		}
+		
+
+	}
+	
+	/**
+	 * Method that creates a singly-linked list version of a given array
+	 * @param array to be serialized
+	 * @return head node of linked list version of array
+	 */
+	public static ListNode listToLinkedList(int[] array) {
+		// head node of resulting linked list
+		ListNode head = new ListNode();
+		
+		// current node for iterating through linked list
+		ListNode current = head;
+		
+		for (int i = 0; i < array.length; i++) {
+			current.val = array[i];
+			current.next = new ListNode();
+			current = current.next;
+		}
+		current.next = null;
+		return head;
 	}
 
 }
